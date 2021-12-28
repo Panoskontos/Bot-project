8f5ff0cf5624d80611e9b156ab08610117fbb20c
Panagiotis Kontos <p.kontos@CORENETWORKS.GR>
2021-12-28 23:28:17 +0200

    underconstruction

--- booking/booking.py
+++ booking/booking.py
@@ -1,4 +1,18 @@
 from selenium import webdriver
+import booking.constants as const
+from booking.constants import BASE_URL
+from selenium.webdriver.common.keys import Keys
+
 
 class Booking(webdriver.Chrome):
-    
+    def __init__(self, driver_path="C:/Users/p.kontos/Downloads/chromedriver.exe", teardown=False):
+        self.driver = webdriver.Chrome(driver_path)
+
+    def land_first_page(self):
+        self.driver.get(const.BASE_URL)
+        self.driver.implicitly_wait(3)
+        self.driver.find_element_by_id('onetrust-accept-btn-handler').click()
+
+    def __exit__(self, *args):
+        if self.teardown:
+            self.driver.quit()
--- run.py
+++ run.py
@@ -1,4 +1,6 @@
-from booking.booking import a
+from booking.booking import Booking
 
 
+with Booking() as bot:
+    bot.land_first_page()
 
